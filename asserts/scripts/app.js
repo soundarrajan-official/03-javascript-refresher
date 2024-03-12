8c6615dd08a12538014d72c64db16d93deb3af22
soundarrajan-official <soundarrajan.official@gmail.com>
2024-03-12 15:32:07 +0530

    Add examples for Control Structures Concepts

---
+++ asserts/scripts/app.js
@@ -0,0 +1,15 @@
+const userName = prompt("Enter Your Name")
+
+if(userName === "Raj") {
+    console.log("Hello " + userName)
+} else if(userName === "John") {
+    console.log("Hello " + userName)
+} else {
+    console.log("Hello Buddy, What's up")
+}
+
+const hobbies = ["Sports", "Travelling"]
+
+for(const hobby of hobbies) {
+    console.log(hobby)
+}
